99263212645a6e00ec7226183f9356259bfc2d0d
Doppel_R <doppelr1001@gmail.com>
2025-08-13 09:47:48 +0200

    update some names, clean Config

--- src/main/java/net/doppelr/lemonmates/LemonMates.java
+++ src/main/java/net/doppelr/lemonmates/LemonMates.java
@@ -1,5 +1,7 @@
-package net.doppelr.lemon_mates;
+package net.doppelr.lemonmates;
 
+import net.doppelr.lemonmates.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -34,6 +36,8 @@ public class LemonMates {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -47,7 +51,9 @@ public class LemonMates {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey()== CreativeModeTabs.FOOD_AND_DRINKS) {
+            event.accept(ModItems.WATERMELONLEMONADE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/doppelr/lemonmates/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.doppelr.lemonmates.item;
+
+import net.doppelr.lemonmates.LemonMates;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(LemonMates.MOD_ID);
+
+    public static final DeferredItem<Item> WATERMELONLEMONADE = ITEMS.register("watermelonlemonade",
+            () -> new Item(new Item.Properties()));
+
+
+
+    public static void register(IEventBus eventBus) {
+         ITEMS.register(eventBus);
+    }
+}
