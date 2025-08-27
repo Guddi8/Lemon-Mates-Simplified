7a4b2878ac99501558a8a9e85c6973c1e615e3e5
Guddi <59566564+Guddi8@users.noreply.github.com>
2025-08-27 14:01:34 +0200

    fixed mod bus

--- src/main/java/net/doppelr/lemonmates/datagen/DataGenerators.java
+++ src/main/java/net/doppelr/lemonmates/datagen/DataGenerators.java
@@ -17,7 +17,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-@EventBusSubscriber(modid = LemonMates.MOD_ID)
+@EventBusSubscriber(modid = LemonMates.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class DataGenerators {
     @SubscribeEvent
     public static void gatherData(GatherDataEvent event) {
