4ce16e7b20278ad1729dace086c638e267b31c56
Doppel_R <doppelr1001@gmail.com>
2025-08-31 18:29:19 +0200

    attempt 68748975 at adding fluids

--- src/main/java/net/doppelr/lemonmates/LemonMates.java
+++ src/main/java/net/doppelr/lemonmates/LemonMates.java
@@ -32,6 +32,7 @@ public class LemonMates {
     public static final String NAME = "Lemon Mates";
     public static final String MOD_ID = "lemonmates";
     public static final Logger LOGGER = LogUtils.getLogger();
+    private static final StackWalker STACK_WALKER = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
@@ -57,7 +58,7 @@ public class LemonMates {
 
     }
 
-    private static final CreateRegistrate REGISTRATE = CreateRegistrate.create(ID)
+    static final CreateRegistrate REGISTRATE = CreateRegistrate.create(ID)
             .defaultCreativeTab((ResourceKey<CreativeModeTab>) null)
             .setTooltipModifierFactory(item ->
                     new ItemDescription.Modifier(item, FontHelper.Palette.STANDARD_CREATE)
@@ -76,5 +77,10 @@ public class LemonMates {
         @SubscribeEvent
         static void onClientSetup(FMLClientSetupEvent event) {
         }
+    }
+    public static CreateRegistrate registrate() {
+        if (!STACK_WALKER.getCallerClass().getPackageName().startsWith("com.simibubi.create"))
+            throw new UnsupportedOperationException("Other mods are not permitted to use create's registrate instance.");
+        return REGISTRATE;
     }
 }
