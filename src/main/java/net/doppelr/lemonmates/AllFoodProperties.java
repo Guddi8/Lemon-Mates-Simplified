8ed01e9e021de9ec003bf83e53280cad5afa22e4
Guddi <59566564+Guddi8@users.noreply.github.com>
2025-09-27 16:59:40 +0200

    Update AllFoodProperties.java

--- src/main/java/net/doppelr/lemonmates/AllFoodProperties.java
+++ src/main/java/net/doppelr/lemonmates/AllFoodProperties.java
@@ -1,7 +1,5 @@
 package net.doppelr.lemonmates;
 
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -10,45 +8,39 @@ import net.neoforged.neoforge.registries.DeferredItem;
 public class AllFoodProperties {
     public static Item.Properties lemonadeDrinks(DeferredItem<Item> usingConvertsTo) {
         return new Item.Properties().food(new FoodProperties.Builder()
-            .nutrition(1)
-            .saturationModifier(0.1f)
+            .nutrition(5)
+            .saturationModifier(1f)
             .alwaysEdible()
-            .effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 400), 0.35f)
             .usingConvertsTo(() -> new ItemStack(usingConvertsTo.get()).getItem())
             .alwaysEdible()
             .build());
     }
 
-    public static final FoodProperties RASPBERRY = new FoodProperties.Builder()
-            .nutrition(1)
-            .saturationModifier(0.25f)
-            .fast()
-            .build();
-
     public static final FoodProperties ORANGE = new FoodProperties.Builder()
-            .nutrition(2)
-            .saturationModifier(0.3f)
-            .build();
+        .nutrition(4)
+        .saturationModifier(0.3f)
+        .build();
 
     public static final FoodProperties ORANGE_SLICE = new FoodProperties.Builder()
-            .nutrition(1)
-            .saturationModifier(0.3f)
-            .fast()
-            .build();
+        .nutrition(2)
+        .saturationModifier(0.3f)
+        .fast()
+        .build();
 
     public static final FoodProperties CITRON = new FoodProperties.Builder()
-            .nutrition(2)
-            .saturationModifier(0.3f)
-            .build();
+        .nutrition(4)
+        .saturationModifier(0.3f)
+        .build();
 
     public static final FoodProperties CITRON_SLICE = new FoodProperties.Builder()
-            .nutrition(1)
-            .saturationModifier(0.3f)
-            .fast()
-            .build();
+        .nutrition(2)
+        .saturationModifier(0.3f)
+        .fast()
+        .build();
 
-    public static final FoodProperties JUICE_DRINK = new FoodProperties.Builder()
-            .nutrition(3)
-            .saturationModifier(0.3f)
+    public static final FoodProperties RASPBERRY = new FoodProperties.Builder()
+            .nutrition(2)
+            .saturationModifier(0.1f)
+            .fast()
             .build();
 }
